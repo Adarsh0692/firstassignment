b4529c93ef6d64f62e4ebbaae8545462ca8a1e91
my-user <my@email.com>
2023-03-07 23:19:39 +0530

    second assignment done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import { Component1,Component2, Component3, Component4 } from './component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Component1/>
+     
+      <br/>
+      <Component2/>
+      <br/>
+      <br/>
+      <Component3/>
+      <br/><br/>
+      <Component4/>
+     </div>
   );
 }
 
---
+++ src/component.js
@@ -0,0 +1,22 @@
+
+
+export function Component1(){
+    return (
+        <h1>Register here...</h1>
+    )
+}
+export function Component2(){
+    return (
+        <input placeholder='mobile...'/>
+    )
+}
+export function Component3(){
+    return (
+        <input placeholder='Password...'/>
+    )
+}
+export function Component4(){
+    return (
+        <button>Submit</button>
+    )
+}
