231a49be11a97afdbdc4a3797df9ef8b8e3b09f4
Adarsh kushwha <adarshkushwaha692@gmail.com>
2023-04-14 21:34:19 +0530

    routing master class seesion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components";
+import {
+  AdminDashboard,
+  CreateProductPage,
+  HardwarePage,
+  Home,
+  Login,
+  Register,
+  SoftwarePage,
+  UpdateProductPage,
+} from "./pages";
+import ProductDetails from "./pages/admin/ProductDetails";
+
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />}>
+            <Route path="" element={<CreateProductPage />} />
+            <Route path="products/create" element={<CreateProductPage />}>
+              <Route path="software" element={<SoftwarePage />}></Route>
+              <Route path="hardware" element={<HardwarePage />}></Route>
+            </Route>
+            <Route
+              path="products/update"
+              element={<UpdateProductPage />}
+            ></Route>
+            <Route
+              path="products/details/"
+              element={<ProductDetails />}
+            ></Route>
+            <Route
+              path="products/details/:productId"
+              element={<ProductDetails />}
+            ></Route>
+          </Route>
+        </Routes>
+      </main>
+
+      <footer>Footer</footer>
+    </Fragment>
   );
 }
 
